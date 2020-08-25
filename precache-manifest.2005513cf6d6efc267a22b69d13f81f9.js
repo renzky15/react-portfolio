d0752c93901eaf770345415c1d568ef7d57192e9
renzky15 <renzowensantillan@gmail.com>
2020-08-25 14:07:02 +0800

    Updates

---
+++ precache-manifest.2005513cf6d6efc267a22b69d13f81f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898fc4080ded00d64387b166d208749c",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "18f1ba728a742f32c12c",
+    "url": "/react-portfolio/static/css/main.d4313049.chunk.css"
+  },
+  {
+    "revision": "8e08d88349028c68313e",
+    "url": "/react-portfolio/static/js/2.1659cbd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.1659cbd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18f1ba728a742f32c12c",
+    "url": "/react-portfolio/static/js/main.aa1db07a.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
